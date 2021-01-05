97a029d4ed3962559b96e10440f492110c1ad4cb
qpongratz <lagarathan@gmail.com>
2021-01-05 11:01:36 -0600

    Get player name and initialize game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,20 @@
 class Player
-  def initialize(playerName, piece)
-    @playerName = playerName
+  def initialize(piece)
     @piece = piece
+    self.get_player_name
   end
+  def get_player_name
+    puts "What would the player of #{@piece}'s like to be called?"
+    @player_name = gets.chomp
+  end
+end
+
+class Game
+  def initialize
+    player_x = Player.new('X')
+    player_o = Player.new('O')
+  end
+end
+
+class Board
 end
