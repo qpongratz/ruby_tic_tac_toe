08651005bc3ea95380febb819d5e9b827786c0eb
qpongratz <lagarathan@gmail.com>
2021-01-05 11:39:30 -0600

    Add get_move for Player class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,18 @@
 class Player
+  
   def initialize(piece)
     @piece = piece
     self.get_player_name
   end
+
   def get_player_name
     puts "What would the player of #{@piece}'s like to be called?"
     @player_name = gets.chomp
   end
+
+  def get_move
+    puts "#{player_name}'s turn. On what space would you like a #{piece}?"
+    gets.chomp #eventually pass to a check with board once I have that setup
 end
 
 class Game
@@ -17,11 +23,12 @@ class Game
     self.board = Board.new
     @players = [(Player.new('X')), (Player.new('O'))]
     @players.shuffle!
+    self.new_turn
   end
 
   def new_turn
     @turn_count += 1
-    
+    @players[@turn_count % 2].get_move
   end
 end
 
