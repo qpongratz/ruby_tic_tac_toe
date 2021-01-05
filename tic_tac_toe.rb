66992866233fcc614a33e4c3c985972f2046a493
qpongratz <lagarathan@gmail.com>
2021-01-05 15:46:04 -0600

    Completed check winner method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -15,10 +15,10 @@ end
 
 class Player
   include Helper
-  attr_accessor :board
   
-  def initialize(piece)
+  def initialize(piece, board)
     @piece = piece
+    @board = board
     self.get_player_name
   end
 
@@ -30,30 +30,38 @@ class Player
   def get_move
     puts "#{@player_name}'s turn. On what space would you like a #{@piece}?"
     input = Helper.translate(gets.chomp)
-    if input.nil? || self.board.invalid_move?(input)
+    if input.nil? || @board.invalid_move?(input)
       puts "Invalid entry."
       self.get_move
     else
-      self.board.update_board(input, @piece)
+      @board.update_board(input, @piece)
     end
+  end
+
+  def declare_winner
+    puts "Congratulations, #{@player_name}! You have won!"
   end
 end
 
 class Game
-  attr_accessor :board
+  attr_accessor :board, :players
   
   def initialize
-    @turn_count = 0
+    @@turn_count = 0
     self.board = Board.new
-    Player.board = self.board
-    @players = [(Player.new('X')), (Player.new('O'))]
-    @players.shuffle!
-    self.new_turn
+    @@players = [(Player.new('X', self.board)), (Player.new('O', self.board))]
+    @@players.shuffle!
+    self.board.display_board
+    Game.new_turn
   end
 
-  def new_turn
-    @turn_count += 1
-    @players[@turn_count % 2].get_move
+  def self.new_turn
+    @@turn_count += 1
+    @@players[@@turn_count % 2].get_move
+  end
+  def self.end_game
+    @@players[@@turn_count % 2].declare_winner
+    "Thank you for playing. Come again soon."
   end
 end
 
@@ -92,12 +100,11 @@ class Board
       [@board_state[2], @board_state[5], @board_state[8]].all?(piece) ||
       [@board_state[0], @board_state[4], @board_state[8]].all?(piece) ||
       [@board_state[2], @board_state[4], @board_state[6]].all?(piece))
-        puts "Winner!"
+        Game.end_game
+    else
+      Game.new_turn
     end  
   end
 end
 
-board = Board.new
-board.update_board(8, "X")
-board.update_board(4, "X")
-board.update_board(0, "X")
+new_game = Game.new
